342c0289caa91b08ec1ecc4409c7b650bbe01ab5
Alexandr <alexandrlachin989@gmail.com>
2025-09-13 08:49:46 +0300

    no comment

--- src/data/echo.cpp
+++ src/data/echo.cpp
@@ -16,15 +16,14 @@ using namespace std;
 char getch_no_echo() {
     char buf = 0;
     struct termios old = {0};
-    fflush(stdout); // Очистить буфер вывода
+    fflush(stdout); 
 
-    tcgetattr(STDIN_FILENO, &old); // Получить текущие настройки терминала
+    tcgetattr(STDIN_FILENO, &old);
     struct termios new_t = old;
-    new_t.c_lflag &= ~ECHO; // Отключить эхо-символов
-    tcsetattr(STDIN_FILENO, TCSANOW, &new_t); // Применить новые настройки
-
-    read(STDIN_FILENO, &buf, 1); // Прочитать один символ
-    tcsetattr(STDIN_FILENO, TCSANOW, &old); // Восстановить исходные настройки
+    new_t.c_lflag &= ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_t);
+    read(STDIN_FILENO, &buf, 1);
+    tcsetattr(STDIN_FILENO, TCSANOW, &old);
 
     return buf;
 }
@@ -35,7 +34,7 @@ std::string cpass(const std::string& prompt){
 
         std::cout << prompt;
         do {
-            ch = getch_no_echo(); // Читаем символ без эха
+            ch = getch_no_echo();
             if (ch != '\r' && ch != '\n') {
                 password += ch;
                 std::cout << '*';
